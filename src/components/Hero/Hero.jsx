1fc2cbd20054239023ed093e2a59f8f1abc44409
Makeen Khawajkeh <makeenkh2004@gmail.com>
2025-06-13 10:41:15 +0300

    first add

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import "../style.css"
 const Hero = ({id}) => {
 return (
     <div className="hero h-screen relative section border-b-2 border-black dark:border-white border-dashed" id={id}>
-        <img src="/public/image/LInes.png" alt="" className="absolute top-[220px]  w-full md:block hidden h-[400px] "/>
+        <img src="/public/image/lines.png" alt="" className="absolute top-[220px]  w-full md:block hidden h-[400px] "/>
         <div className="container m-auto " >
             <div className="hero-content relative top-[140px] md:top-[240px] flex justify-around flex-col md:flex-row md:gap-0 gap-10 ">
                 <div className="left-side lg:mt-[50px] text-center lg:text-start md:text-left relative lg:w-[638px] md:w-[465px]">
@@ -12,8 +12,8 @@ return (
                 </div>
                 <div className="right-side relative md:top-0 top-16 ">
                     <div className="images relative">
-                        <img src="/public/image/image (4).png" alt="" className="w-[380px]  relative z-10 left-2/4 translate-x-[-50%] -top-[45px] sm:w-[380px] md:w-[380px] lg:w-[430px]"/>
-                        <img src="/public/image/Circle.png" alt="" className="absolute top-0 left-2/4 translate-x-[-50%] w-[365px]  md:w-[350px] lg:w-[420px]"/>
+                        <img src="/public/image/my-image.png" alt="" className="w-[380px]  relative z-10 left-2/4 translate-x-[-50%] -top-[45px] sm:w-[380px] md:w-[380px] lg:w-[430px]"/>
+                        <img src="/public/image/circle.png" alt="" className="absolute top-0 left-2/4 translate-x-[-50%] w-[365px]  md:w-[350px] lg:w-[420px]"/>
                         <img src="/public/image/squers.png" alt="" className="absolute top-0 left-2/4 translate-x-[-50%] w-[365px]  md:w-[350px] lg:w-[420px]" />
                     </div>
                 </div>
